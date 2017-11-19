24d82506ca3e258e1b034d1647ec23f2c53bff56
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-11-19 13:14:30 +0000

    Add colour scheme creation modal

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,12 +1,25 @@
 import { Modal } from './modal';
 import { SimpleButton } from './simple-button';
+import './lib/jscolor.min.js';
 
+
+// Issue page
 let issueCreateModal = new Modal('issue-create-modal');
 
 let createIssueBtn = new SimpleButton('create-issue', (event) => {
   issueCreateModal.show();
 });
 
+
+// Data page
+let colourSchemeCreateModal = new Modal('colour-scheme-create-modal');
+
+let colourSchemeCreateBtn = new SimpleButton('create-colour-scheme', (event) => {
+  colourSchemeCreateModal.show();
+});
+
+
+// All pages
 let logoutBtn = new SimpleButton('logout-btn', (event) => {
   window.location = '/logout';
 });
