536d047d48646978dc817e8c2f762d626f454129
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-12-04 20:11:31 +0000

    Repurpose token model

--- src/server/models/token-model.js
+++ src/server/models/token-model.js
@@ -1,55 +1,22 @@
 import db from '../database';
+import jwt from 'jsonwebtoken';
+import config from '../config';
+
+import { UserModel } from './user-model';
 
 export class TokenModel {
-  constructor(id, access, refresh, accessExpiry, refreshExpiry) {
-    this.id = id;
-    this.access = access;
-    this.refresh = refresh;
-    this.accessExpiry = accessExpiry;
-    this.refreshExpiry = refreshExpiry;
-  }
+  constructor() {}
 
-  static createFromReq({access, refresh, accessExpiry, refreshExpiry}) {
-    let id = -1;
-    return new TokenModel(id, access, refresh, accessExpiry, refreshExpiry);
-  }
+  static generateTokenForUser(user) {
+    if (!(user instanceof UserModel)) throw new Error('Data must be of type UserModel');
 
-  static createFromDb(tokenData) {
-    return new TokenModel(
-      tokenData.TOKEN_ID,
-      tokenData.ACCESS_TOKEN,
-      tokenData.REFRESH_TOKEN,
-      tokenData.ACCESS_EXPIRY,
-      tokenData.REFRESH_EXPIRY
-    );
-  }
+    let payload = {
+      id: user.id,
+      hashedPass: user.hashedPass
+    }
 
-  static async insert(token) {
-    if (!(token instanceof TokenModel)) throw new Error('Data must be of type TokenModel');
-    let sql = 'INSERT INTO tokens VALUES (NULL, ?, ?, ?, ?)';
-    let inserts = [
-      token.access,
-      token.refresh,
-      token.accessExpiry,
-      token.refreshExpiry
-    ];
-    
-    sql = db.format(sql, inserts);
-    let result = await db.query(sql);
-    token.id = result.insertId;
+    let token = jwt.sign(payload, config.secret, { expiresIn: '1h' });
 
-    // if result is ok return the token
     return token;
-  }
-
-  static async getAll() {
-    let results = await db.query('SELECT * FROM tokens');
-    return results.map(this.createFromDb);
-  }
-
-  static async generateBlankToken() {
-    let token = new TokenModel(-1, '', '', new Date(), new Date());
-    let insertedToken = await this.insert(token);
-    return insertedToken;
   }
 }
