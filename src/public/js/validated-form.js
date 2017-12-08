1c6d9142af798cfb98b6370eb3fca0511a57c11f
Sullivan Ford <sullyjhf@hotmail.co.uk>
2017-12-08 12:50:10 +0000

    Catch more errors in clientside validatedform

--- src/public/js/validated-form.js
+++ src/public/js/validated-form.js
@@ -11,12 +11,16 @@ export class ValidatedForm {
 
   showErrors() {
     for(let [name, value] of Object.entries(this.formData)) {
+      if (typeof value !== 'string') continue;
       let input = this.elm.querySelectorAll(`[name=${name}]`)[0];
+      if (!input) continue;
       input.value = value;
     }
 
     for (let [name, value] of Object.entries(this.errors)) {
+      if (typeof value !== 'string') continue;
       let input = this.elm.querySelectorAll(`[name=${name}]`)[0];
+      if (!input) continue;
       let parent = input.parentElement;
       parent.classList.add('error');
       parent.getElementsByClassName('error-icon')[0].setAttribute('data-before', value);
